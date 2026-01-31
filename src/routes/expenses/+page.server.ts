02f985fdd37eed8a0643ba90668aeb45b63ddd71
subZero <marvinfordan08@gmail.com>
2026-01-31 15:51:29 +0800

    expenses - conform query string params with backedn naming conventions

--- src/routes/expenses/+page.server.ts
+++ src/routes/expenses/+page.server.ts
@@ -4,18 +4,18 @@ import type { Expense } from "$lib/types/Expense";
 import type { PaginatedResponse } from "$lib/types/Paginated";
 
 export const load: PageServerLoad = async ({ url }) => {
-  const expenseDate = url.searchParams.get("date") ?? "";
+  const expenseDate = url.searchParams.get("expense_date") ?? "";
   const sort = url.searchParams.get("sort");
   const fields = String(url.searchParams.get("fields") ?? "");
-  const pageNumber = Number(url.searchParams.get("page") ?? 1);
-  const pageSize = Number(url.searchParams.get("pageSize") ?? 10);
+  const page = Number(url.searchParams.get("page") ?? 1);
+  const pageSize = Number(url.searchParams.get("page_size") ?? 10);
 
   const response = await get<PaginatedResponse<Expense>>("/api/expenses", {
     params: {
       expenseDate,
       sort,
       fields,
-      page: pageNumber,
+      page,
       pageSize,
     },
   });
